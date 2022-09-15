99f41abbe182d3a83030e8bff4e53ab473d0afdb
dymasius <dymasius12@gmail.com>
2022-09-16 02:41:35 +0800

    15:38 creating the search.py to get more info from tweet data

--- search.py
+++ search.py
@@ -6,6 +6,8 @@ client = tweepy.Client(bearer_token=config.BEARER_TOKEN)
 # query = 'covid OR covid19 -is:retweet -has:media'
 query = 'covid OR covid19 -is:retweet -has:media'
 
-response = client.search_recent_tweets(query=query, max_results=100)
+response = client.search_recent_tweets(query=query, max_results=100, tweet_fields=['created_at', 'lang'], expansions=['author_id'])
 
-print(response)
+for tweet in response.data:
+    print(tweet.id)
+    print(tweet.lang)
